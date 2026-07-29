c7122a7523a4771abbe3333d0bbe02a4d2691bf3
vtu30598-byte <vtu30598@veltech.edu.in>
2026-07-29 21:16:21 +0530

    Add files via upload

---
+++ print.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public Class print
+{
+
+Scanner sc = new Scanner(System.in);
+void printI()
+{
+int i= sc.nextInt();
+}
+void printf()
+{
+Float F=sc.nextFloat();
+System.out.println("Enter the float values")
+void print sc
+{
+string s = sc.next int()
+{
+Scanner obj = new Scanner(system.in);
+a=obj.nextint();
+f=obj.nextfloat();
+c=obj.nextline();
+s=obj.nextline();
+}
+}
